1233790659bb200cfe4571892becf73d74a84948
Michael Downs <mdowns@g.harvard.edu>
2020-05-01 02:45:46 +0000

    improved script that separates and cleans project gutenberg texts

--- bin/make_gutenberg_subset.py
+++ bin/make_gutenberg_subset.py
@@ -3,6 +3,7 @@ import zipfile
 import zlib
 
 import fire
+from gutenberg_cleaner import simple_cleaner, super_cleaner
 import pandas as pd
 import tqdm
 
@@ -17,12 +18,13 @@ PG_HEADER = [
 
 
 PG_FOOTER = [
+              "end of project gutenberg's",
               "end of the project gutenberg ebook",
               "end of this project gutenberg ebook"
 ]
 
 
-def remove_header_footer(pgid, data):
+def remove_header_footer_old(pgid, data):
     new_data = []
     past_header = False
     past_footer = False
@@ -48,6 +50,52 @@ def remove_header_footer(pgid, data):
     return ('\n'.join(new_data)).strip().encode('utf-8')
 
 
+skip_lines = [
+     'distributed proofreader',
+     'distributed proofreading',
+     'publisher',
+     'editor',
+     'html',
+     'proofreaders',
+     "transcriber's",
+     'copyright',
+     "the internet archive",
+     "illustration"
+]
+
+def remove_header_footer(pgid, data):
+    try:
+        string = data.decode('utf-8')
+    except UnicodeDecodeError:
+        string = data.decode('cp1252')
+
+    #cleaned_string = simple_cleaner(string) 
+    cleaned_string_lines = super_cleaner(string).strip().split('\n')
+    cleaned_string_lines = [l for l in cleaned_string_lines if l.strip() != "[deleted]"]
+    cleaned_string = ('\n'.join(cleaned_string_lines)).strip()
+    cleaned_string_lines = cleaned_string.split('\n') 
+
+
+    remove_first_line = False
+    remove_last_line = False
+    for l in skip_lines:
+        if l in cleaned_string_lines[0].lower():
+            remove_first_line = True
+   
+    for l in skip_lines:
+        if l in cleaned_string_lines[-1].lower():
+            remove_last_line = True
+   
+    if remove_first_line:
+        cleaned_string_lines = cleaned_string_lines[1:]
+
+    if remove_last_line:
+        cleaned_string_lines = cleaned_string_lines[:-1]
+
+    cleaned_string = '\n'.join(cleaned_string_lines)
+    
+
+    return cleaned_string.strip().encode('utf-8')
 
 def make_genre_subset(genre_file, pg_raw="./data/project_gutenberg_raw.zip", out_zip=None):
     print(f"Reading from {genre_file}")
